ccd17762815122d1484d7f3cc8872a9f273fb0a3
lsprdev <github@lspr.dev>
2024-07-14 17:21:51 -0300

    Learning Control Flow

--- control_flow/lib/control_flow.ex
+++ control_flow/lib/control_flow.ex
@@ -3,16 +3,29 @@ defmodule ControlFlow do
   Documentation for `ControlFlow`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ControlFlow.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def check_max_load(value) do
+    if value <= 55, do: "Safe to fly!", else: "Too heavy to fly!"
   end
+
+  def check_fuel_level(percentage) do
+    cond do
+      percentage == 100 -> "full tank"
+      percentage > 75 -> "3/4 tank"
+      percentage > 49 -> "1/2 tank"
+      percentage > 24 -> "1/4 tank"
+      true -> "Empty tank"
+    end
+  end
+
+  def http_status_code(value) do
+    case value do
+      200 -> :ok
+      201 -> :created
+      202 -> :accepted
+      n when is_integer(n) and n > 299 -> :error
+      _ -> :unknown
+    end
+  end
+
+
 end
