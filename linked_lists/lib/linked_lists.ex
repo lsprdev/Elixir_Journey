a0b16eb53bc08f4e381b1d895d919bf7b6c5f732
lsprdev <github@lspr.dev>
2024-07-14 13:37:37 -0300

    more about lists in elixir

--- linked_lists/lib/linked_lists.ex
+++ linked_lists/lib/linked_lists.ex
@@ -3,8 +3,12 @@ defmodule LinkedLists do
   Documentation for `LinkedLists`.
   """
 
-  def append(list, element) do
+  def append_last(list, element) do
     list ++ [element]
+  end
+
+  def append_first(list, element) do
+    [element] ++ list
   end
 
   def other_append(list, element) do
@@ -20,4 +24,34 @@ defmodule LinkedLists do
     [first, second | rest] = list
     {first, second, rest}
   end
+
+  def car_list do
+    ["volvo", "bmw", "mercedes", "audi"]
+  end
+
+  def add_slow(list, new) do
+    list ++ [new]
+  end
+
+  def add_fast(list, new) do
+    # List.insert_at(list, -1, new)
+    reverse_list = Enum.reverse(list)
+    new_list = [new | reverse_list]
+    Enum.reverse(new_list)
+  end
+
+  def remove_item(list, index) do
+    List.delete_at(list, index)
+  end
+
+  def list_length(list) do
+    # Enum.count(list)
+    length(list)
+  end
+
+  def search_item(list, item) do
+    # Enum.find_index(list, fn x -> x == item end) # index of item in list
+    item in list # true or false
+  end
+
 end
